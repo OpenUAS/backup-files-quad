6da8c436bd9365fb1ef3d1c41284fe75a7990e23
bheliom <bheliom@bheliomC.(none)>
2013-08-26 23:55:08 +0200

    26-08-2013

---
+++ airborne/modules/hs_module/hs071_c.h
@@ -0,0 +1,36 @@
+#ifndef HS_H
+#define HS_H
+
+#include "/home/bheliom/develop/CoinIpopt/build/include/coin/IpStdCInterface.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+void period_hs(void);
+
+Bool eval_f(Index n, Number* x, Bool new_x,
+            Number* obj_value, UserDataPtr user_data);
+
+Bool eval_grad_f(Index n, Number* x, Bool new_x,
+                 Number* grad_f, UserDataPtr user_data);
+
+Bool eval_g(Index n, Number* x, Bool new_x,
+            Index m, Number* g, UserDataPtr user_data);
+
+Bool eval_jac_g(Index n, Number *x, Bool new_x,
+                Index m, Index nele_jac,
+                Index *iRow, Index *jCol, Number *values,
+                UserDataPtr user_data);
+
+Bool eval_h(Index n, Number *x, Bool new_x, Number obj_factor,
+            Index m, Number *lambda, Bool new_lambda,
+            Index nele_hess, Index *iRow, Index *jCol,
+            Number *values, UserDataPtr user_data);
+
+Bool intermediate_cb(Index alg_mod, Index iter_count, Number obj_value,
+                     Number inf_pr, Number inf_du, Number mu, Number d_norm,
+                     Number regularization_size, Number alpha_du,
+                     Number alpha_pr, Index ls_trials, UserDataPtr user_data);
+int ipopt_init(void);
+
+#endif

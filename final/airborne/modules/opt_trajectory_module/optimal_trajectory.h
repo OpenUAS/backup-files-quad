fb4c3642779cc63742e2e390aa822137eb04627e
bheliom <bheliom@bheliomC.(none)>
2013-09-03 16:48:35 +0200

    sprawdz

---
+++ final/airborne/modules/opt_trajectory_module/optimal_trajectory.h
@@ -0,0 +1,36 @@
+/*
+ * optimal_trajectory.h
+ *
+ *  Created on: Aug 21, 2013
+ *      Author: bheliom
+ */
+
+#ifndef OPTIMAL_TRAJECTORY_H_
+#define OPTIMAL_TRAJECTORY_H_
+
+#include "IpStdCInterface.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
+/* Function Declarations */
+Bool eval_f(Index n, Number* x, Bool new_x, Number* obj_value,
+		UserDataPtr user_data);
+
+Bool eval_grad_f(Index n, Number* x, Bool new_x, Number* grad_f,
+		UserDataPtr user_data);
+
+Bool eval_g(Index n, Number* x, Bool new_x, Index m, Number* g,
+		UserDataPtr user_data);
+
+Bool eval_jac_g(Index n, Number *x, Bool new_x, Index m, Index nele_jac,
+		Index *iRow, Index *jCol, Number *values, UserDataPtr user_data);
+
+Bool eval_h(Index n, Number *x, Bool new_x, Number obj_factor, Index m,
+		Number *lambda, Bool new_lambda, Index nele_hess, Index *iRow,
+		Index *jCol, Number *values, UserDataPtr user_data);
+int ipoptMain(void);
+
+extern float ipopt_cmd[3];
+
+#endif /* OPTIMAL_TRAJECTORY_H_ */
